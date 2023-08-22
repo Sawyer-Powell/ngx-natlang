fe55cc7801e0c732b15881c363b90c6418e2776d
Sawyer Powell <sawyerhpowell@gmail.com>
2023-08-22 13:25:46 -0400

    increased path robustness

--- test-build.sh
+++ test-build.sh
@@ -1,9 +1,10 @@
+cd ~/dev/personal/ngx-natlang
 ng build;
-cp -r ./projects/ngx-natlang/bin ./dist/ngx-natural-language;
+cp -r projects/ngx-natlang/bin dist/ngx-natural-language;
 cd dist/ngx-natural-language;
 npm pack --pack-destination ~/dev/personal;
 cd ~/dev/personal/ngx-natlang-test;
 npm uninstall ngx-natural-language;
-npm install ~/dev/personal/ngx-natural-language-0.1.4.tgz;
+npm install ~/dev/personal/ngx-natural-language-0.2.0.tgz;
 rm -rf .angular
 cd ~/dev/personal/ngx-natlang;
