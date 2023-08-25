b66ebf777ca2676067a6a844770398ed6bcebec4
Sawyer Powell <sawyerhpowell@gmail.com>
2023-08-25 14:09:19 -0400

    now version agnostic

--- test-build.sh
+++ test-build.sh
@@ -2,9 +2,10 @@ cd ~/dev/personal/ngx-natlang
 ng build;
 cp -r projects/ngx-natlang/bin dist/ngx-natural-language;
 cd dist/ngx-natural-language;
+rm ~/dev/personal/ngx-natural-language-*.tgz
 npm pack --pack-destination ~/dev/personal;
 cd ~/dev/personal/ngx-natlang-test;
 npm uninstall ngx-natural-language;
-npm install ~/dev/personal/ngx-natural-language-0.2.0.tgz;
+npm install ~/dev/personal/ngx-natural-language-*.tgz;
 rm -rf .angular
 cd ~/dev/personal/ngx-natlang;
